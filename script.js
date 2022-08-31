ec05969a1e5270eede0d8fec18b5f4839c276a85
liciniopsj <licinio.psj@gmail.com>
2022-08-31 17:31:00 -0300

    Requisito 1, 2 e 3

---
+++ script.js
@@ -0,0 +1,6 @@
+let colorBox = document.getElementsByClassName('color');
+
+colorBox[0].style.backgroundColor = 'black';
+colorBox[1].style.backgroundColor = 'red';
+colorBox[2].style.backgroundColor = 'green';
+colorBox[3].style.backgroundColor = 'blue';
