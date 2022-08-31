d5438bac92b6a3f437e09a8ad1308fdf2f5e9e6c
liciniopsj <licinio.psj@gmail.com>
2022-08-31 18:38:31 -0300

    Corrigindo R2 e R3

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const colorBox = document.getElementsByClassName('color');
 const randomColorButton = document.getElementById('button-random-color');
 
-function randomColor(){
-    colorBox[1].style.backgroundColor = randomHexColor();
-    colorBox[2].style.backgroundColor = randomHexColor();
-    colorBox[3].style.backgroundColor = randomHexColor();
-    
-}
-
-function randomHexColor(){
+function randomHexColor() {
     let hexCode = Math.floor(Math.random()*16777215).toString(16);
     let hexResult = "#" + hexCode;
     return hexResult;
+}
+
+function randomColor() {
+    colorBox[1].style.backgroundColor = randomHexColor();
+    colorBox[2].style.backgroundColor = randomHexColor();
+    colorBox[3].style.backgroundColor = randomHexColor();
 }
 
 randomColorButton.addEventListener('click', randomColor);
